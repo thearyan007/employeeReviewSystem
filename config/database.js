4d23222b2c90eb18e361a575fdc3ebaca1ac892e
thearyan007 <thearyangupta007@gmail.com>
2023-09-23 00:19:24 +0530

    final

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// Connection to mongodb.
+mongoose.connect("mongodb://127.0.0.1:27017/employees"); 
+
+
+const db = mongoose.connection; // db store the connection
+
+// cheacking the connection
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("DATABASE connection is Established");
+});
+
+// exporting the connection.
+module.exports = db;
